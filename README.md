830637f50ee950f9211bbae1201efeb5d8f3d81a
Yahya Ghrab <yghrab@dial-once.com>
2023-01-11 15:43:12 +0100

    multi param management improvement

--- README.md
+++ README.md
@@ -19,28 +19,32 @@ Second, you need to update the files of the sub-project you want to exeucte by a
 
 You can generate a test/train split in rasa using the command `rasa data split nlu` from you rasa project root directory. Than copy/paste them in files.
 
-`config.yml` should be added as a template as in the example, where you replace the values with the their name.
-
 ## Usage
 
 The code is organized as a `MLFLOW Project`. It contains a complete workflow that spans a search space in search for best parameters and leverages multithreading for faster execution.
 
 You can use this project in different ways:
 
-- By running from root directory: `mlflow run. <pass you own params if needed>` .
-- If you don't want to clone this project, you canrun this command :
+* By running from root directory with default params read `files/`:
+  * `mlflow run .`
 
-  - without default params: `mlflow run git@github.com:YahyaGrb/mlflow_rasa_track.git`
-  - with your own params/files: `mlflow run git@github.com:YahyaGrb/mlflow_rasa_track.git -P <your params if needed>`
-- From a jupyter notebook:track
+- By running from root directory and passing custom files:
+  - `mlflow run . -P config="path/to/config" -P validation_data="path/to/val/data" -P training_data="path/to/train/data"`
 
-  ```
-  import mlflow
-  project_uri = "https://github.com/YahyaGrb/mlops_rasa/mlflow_rasa_hp"
-  params = your_params # (as needed by the project)
+If you don't want to clone this project, you canrun this command :
 
-  mlflow.run(project_uri, parameter=params)
-  ```
+- without default params: `mlflow run git@github.com:YahyaGrb/mlflow_rasa_track.git`
+- with your own params/files: `mlflow run git@github.com:YahyaGrb/mlflow_rasa_track.git -P param1=val1 -P param2=val2`
+
+From a jupyter notebook:track
+
+```
+import mlflow
+project_uri = "https://github.com/YahyaGrb/mlops_rasa/mlflow_rasa_hp"
+params = your_params # (as needed by the project)
+
+mlflow.run(project_uri, parameter=params)
+```
 
 ## Results
 
--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import sys
 import mlflow
 import logging
 import click
-import tempfile
-from src.utils import _transform_uri_to_path, _get_run_config
+from src.utils import _transform_uri_to_path
 from mlflow.tracking import MlflowClient
 
 
@@ -27,23 +26,20 @@ def _get_or_run(entrypoint, parameters, synchronous=True): #removed git commit a
     return MlflowClient().get_run(submitted_run.run_id)
 
 @click.command(help="Perform unique run of the code with provided params.")
-@click.option('--params', default = {"epochs": 10, "threshold": 0.3, "model": ""})
-@click.argument("config_template", default="files/template_config.yml")
-@click.argument("train_data", default="files/training_data.yml")
-@click.argument("validation_data", default="files/test_data.yml")
-def  workflow(params, config_template,train_data, validation_data):
+@click.option("--config", default="files/template_config.yml")
+@click.option("--train-data", default="files/training_data.yml")
+@click.option("--validation-data", default="files/test_data.yml")
+def  workflow(config,train_data,validation_data):
     with mlflow.start_run():
         mlflow.set_experiment("track_rasa")
-        with tempfile.TemporaryDirectory() as temp_config_dir:
-            generated_config =_get_run_config(config_template, params, temp_config_dir)
-            logger.info("Starting to train")
-            train_model = _get_or_run("train", {"config":generated_config, "training":train_data})
-            logger.info("Training complete")
-            model_uri = os.path.join(train_model.info.artifact_uri, "model")
-            model_path = _transform_uri_to_path(model_uri)
-            logger.info("Starting to test")
-            test_model = _get_or_run("test", {"model_path": model_path, "validation": validation_data})
-            logger.info("Testing complete")
+        logger.info("Starting to train")
+        train_model = _get_or_run("train", {"config":config, "training":train_data})
+        logger.info("Training complete")
+        model_uri = os.path.join(train_model.info.artifact_uri, "model")
+        model_path = _transform_uri_to_path(model_uri)
+        logger.info("Starting to test")
+        test_model = _get_or_run("test", {"model_path": model_path, "validation": validation_data})
+        logger.info("Testing complete")
 
 
 if __name__ == "__main__":
